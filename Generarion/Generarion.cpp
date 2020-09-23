e38d3bb9c61f30f1606b3adec72fda724a1ade11
BraveHunter2001 <64270977+BraveHunter2001@users.noreply.github.com>
2020-09-23 19:44:18 +0300

    time add

--- Generarion/Generarion.cpp
+++ Generarion/Generarion.cpp
@@ -3,6 +3,7 @@
 #include <Windows.h>
 #include <stdlib.h>
 #include <time.h>
+#include <fstream>
 
 const short SHIFT = 192;
 const short LENOFLETTERS = 32;
@@ -20,6 +21,17 @@ bool isInSet(unsigned char item, unsigned char* set) {
     }
 
     return false;
+}
+
+void InputSet(unsigned char* set) {
+    char symbol;
+    int i = 0;
+    do {
+        cin >> symbol;
+        if (symbol >= 'А' && symbol <= 'Я' && symbol != 'Ё' && !isInSet(symbol, set)) {
+            set[i++] = symbol;
+        }
+    } while (cin.peek() != '\n');
 }
 
 void DeleteItem(int index, unsigned char* set) {
@@ -88,7 +100,7 @@ bool isInSet(unsigned char item, ST* set) {
     return false;
 }
 
-ST* DeleteItem(unsigned char item, ST* set) {
+void DeleteItem(unsigned char item, ST*& set) {
     ST* x = set;
     if (x->letter == item) {
         set = x->next;
@@ -111,34 +123,28 @@ ST* DeleteItem(unsigned char item, ST* set) {
             }
         }
     }
-    return set;
 }
 
-ST* AddItem(unsigned char item, ST* set) {
+void AddItem(unsigned char item, ST*& set) {
     set = new ST(item, set);
-    return set;
 }
 
-ST* UnionSets(ST* source, ST* destination) {
-    ST* s = source, * d = destination;
-    while (s) {
-        if (!isInSet(s->letter, d)) {
-            d = AddItem(s->letter, d);
+void UnionSets(ST* source, ST*& destination) {
+    while (source) {
+        if (!isInSet(source->letter, destination)) {
+            AddItem(source->letter, destination);
         }
-        s = s->next;
+        source = source->next;
     }
-    return d;
 }
 
-ST* ExclusionSets(ST* source, ST* destination) {
-    ST* s = source, * d = destination;
-    while (s) {
-        if (isInSet(s->letter, d)) {
-            d = DeleteItem(s->letter, d);
+void ExclusionSets(ST* source, ST*& destination) {
+    while (source) {
+        if (isInSet(source->letter, destination)) {
+            DeleteItem(source->letter, destination);
         }
-        s = s->next;
+        source = source->next;
     }
-    return d;
 }
 
 void PrintSet(ST* set) {
@@ -160,17 +166,13 @@ void ToBool(unsigned char* set, bool* universum) {
 
 void UnionSets(bool* source, bool* destination) {
     for (size_t i = 0; i < LENOFLETTERS; ++i) {
-        if (source[i] != destination[i]) {
-            destination[i] = true;
-        }
+        destination[i] = destination[i] | source[i];
     }
 }
 
 void ExclusionSets(bool* source, bool* destination) {
     for (size_t i = 0; i < LENOFLETTERS; ++i) {
-        if (source[i] == destination[i]) {
-            destination[i] = false;
-        }
+        destination[i] = destination[i] & ~source[i];
     }
 }
 
@@ -189,39 +191,17 @@ void PrintSet(bool* set) {
 int ToMachineWord(unsigned char* set) {
     int mword = 0;
     for (size_t i = 0; set[i]; ++i) {
-        mword = mword | ONE << (int)(set[i] - SHIFT);
+        mword = mword | ONE << (set[i] - SHIFT);
     }
-    return mword;
-}
-
-bool isInSet(int index, int mword) {
-    return mword & ONE << index;
-}
-
-int DeleteItem(int index, int mword) {
-    mword = mword & ~(ONE << index);
-    return mword;
-}
-
-int AddItem(int index, int mword) {
-    mword = mword | ONE << index;
     return mword;
 }
 
 void UnionSets(int source, int& destination) {
-    for (size_t i = 0; i < LENOFLETTERS; ++i) {
-        if (isInSet(i, source)) {
-            destination = AddItem(i, destination);
-        }
-    }
+    destination = source | destination;
 }
 
 void ExclusionSets(int source, int& destination) {
-    for (size_t i = 0; i < LENOFLETTERS; ++i) {
-        if (isInSet(i, source)) {
-            destination = DeleteItem(i, destination);
-        }
-    }
+    destination = ~source & destination;
 }
 
 void PrintSet(int mword) {
@@ -232,9 +212,7 @@ void PrintSet(int mword) {
         }
     }
 }
-
-#pragma endregion all what neded for word
-
+#pragma endregion
 #pragma region Random generators
 /*
 * Первая версия генератора для машинного слова. 
@@ -299,12 +277,6 @@ int main()
     unsigned char D[LENOFLETTERS + 1] = { '\0' };
     unsigned char E[LENOFLETTERS + 1] = { '\0' };
 
-    ST* A_List = nullptr;
-    ST* B_List = nullptr;
-    ST* C_List = nullptr;
-    ST* D_List = nullptr;
-    ST* E_List = nullptr;
-
     bool A_Bool[LENOFLETTERS] = { false };
     bool B_Bool[LENOFLETTERS] = { false };
     bool C_Bool[LENOFLETTERS] = { false };
@@ -318,123 +290,188 @@ int main()
     int E_Mword = 0;
 
 #pragma region Время длителности
-    clock_t t;
-    int countIteartion = 10000;
-
-    t = clock();
-    for (int i = 0; i < countIteartion; i++)
-    {
-        Generator(15, A);
-        Generator(15, B);
-        Generator(15, C);
-        Generator(15, D);
-
-        UnionSets(A, E);
-        ExclusionSets(B, E);
-        ExclusionSets(C, E);
-        ExclusionSets(D, E);
-
-        Discharge(A);
-        Discharge(B);
-        Discharge(C);
-        Discharge(D);
-        Discharge(E);
-    }
-    t = clock() - t;
-    cout <<"Array: "<< (float)t / CLK_TCK / countIteartion << endl;
-
-   
-
-    t = clock();
-    for (size_t i = 0; i < countIteartion; i++)
-    {
-        Generator(15, A);
-        Generator(15, B);
-        Generator(15, C);
-        Generator(15, D);
-
-        A_List = ToList(A);
-        B_List = ToList(B);
-        C_List = ToList(C);
-        D_List = ToList(D);
-
-        E_List = UnionSets(A_List, E_List);
-
-        E_List = ExclusionSets(B_List, E_List);
-        E_List = ExclusionSets(C_List, E_List);
-        E_List = ExclusionSets(D_List, E_List);
-
-        Discharge(A);
-        Discharge(B);
-        Discharge(C);
-        Discharge(D);
-        Discharge(E);
-    }
-    t = clock() - t;
+    clock_t t, sumt = 0;
     
-    cout << "List: " << (float)t / CLK_TCK/countIteartion << endl;
+    int countIteartion = 1;
+    const int count = 8;
+    int arCount[count];
+    double timeArray[count];
+    double timeList[count];
+    double timeBool[count];
+    double timeMachine[count];
+  
 
-
-    t = clock();
-    for (size_t i = 0; i < countIteartion; i++)
+    for (size_t i = 1; i <= count; i++)
     {
-        Generator(15, A);
-        Generator(15, B);
-        Generator(15, C);
-        Generator(15, D);
+        
+        arCount[i-1] = countIteartion;
+        cout << "Count:" << i << endl;
+        for (int i = 0; i < countIteartion; i++)
+        {
+            Generator(15, A);
+            Generator(15, B);
+            Generator(15, C);
+            Generator(15, D);
+            t = clock();
+            UnionSets(A, E);
+            ExclusionSets(B, E);
+            ExclusionSets(C, E);
+            ExclusionSets(D, E);
+            sumt += clock() - t;
+            Discharge(A);
+            Discharge(B);
+            Discharge(C);
+            Discharge(D);
+            Discharge(E);
+        }
+        
+        cout << " Array: " << (double)sumt << endl;
+        timeArray[i-1] = (double)sumt / CLK_TCK;
+        sumt = 0;
+        
+        for (size_t i = 0; i < countIteartion; i++)
+        {
+            ST* A_List = nullptr;
+            ST* B_List = nullptr;
+            ST* C_List = nullptr;
+            ST* D_List = nullptr;
+            ST* E_List = nullptr;
+            Generator(15, A);
+            Generator(15, B);
+            Generator(15, C);
+            Generator(15, D);
 
-        ToBool(A, A_Bool);
-        ToBool(B, B_Bool);
-        ToBool(C, C_Bool);
-        ToBool(D, D_Bool);
+            A_List = ToList(A);
+            B_List = ToList(B);
+            C_List = ToList(C);
+            D_List = ToList(D);
+            t = clock();
+            UnionSets(A_List, E_List);
 
-        UnionSets(A_Bool, E_Bool);
+            ExclusionSets(B_List, E_List);
+            ExclusionSets(C_List, E_List);
+            ExclusionSets(D_List, E_List);
+            sumt += clock() - t;
+            Discharge(A);
+            Discharge(B);
+            Discharge(C);
+            Discharge(D);
+            Discharge(E);
 
-        ExclusionSets(B_Bool, E_Bool);
-        ExclusionSets(C_Bool, E_Bool);
-        ExclusionSets(D_Bool, E_Bool);
+            delete A_List;
+            delete B_List;
+            delete C_List;
+            delete D_List;
+            delete E_List;
+        }
+       
 
-        Discharge(A);
-        Discharge(B);
-        Discharge(C);
-        Discharge(D);
-        Discharge(E);
+        cout << " List: " << (double)sumt << endl;
+        timeList[i-1] = (double)sumt / CLK_TCK;
+        sumt = 0;
+       
+        for (size_t i = 0; i < countIteartion; i++)
+        {
+
+            Generator(15, A);
+            Generator(15, B);
+            Generator(15, C);
+            Generator(15, D);
+
+            ToBool(A, A_Bool);
+            ToBool(B, B_Bool);
+            ToBool(C, C_Bool);
+            ToBool(D, D_Bool);
+            t = clock();
+            UnionSets(A_Bool, E_Bool);
+
+            ExclusionSets(B_Bool, E_Bool);
+            ExclusionSets(C_Bool, E_Bool);
+            ExclusionSets(D_Bool, E_Bool);
+            sumt += clock() - t;
+            Discharge(A);
+            Discharge(B);
+            Discharge(C);
+            Discharge(D);
+            Discharge(E);
+
+        }
+
+       
+        cout << " Boolean array: " << (double)sumt<< endl;
+        timeBool[i-1] = (double)sumt / CLK_TCK;
+        sumt = 0;
+        
+        for (size_t i = 0; i < countIteartion; i++)
+        {
+            Generator(15, A);
+            Generator(15, B);
+            Generator(15, C);
+            Generator(15, D);
+
+            A_Mword = ToMachineWord(A);
+            B_Mword = ToMachineWord(B);
+            C_Mword = ToMachineWord(C);
+            D_Mword = ToMachineWord(D);
+            t = clock();
+            UnionSets(A_Mword, E_Mword);
+
+            ExclusionSets(B_Mword, E_Mword);
+            ExclusionSets(C_Mword, E_Mword);
+            ExclusionSets(D_Mword, E_Mword);
+
+            sumt += clock() - t;
+            Discharge(A);
+            Discharge(B);
+            Discharge(C);
+            Discharge(D);
+            Discharge(E);
+
+            A_Mword = 0;
+            B_Mword = 0;
+            C_Mword = 0;
+            D_Mword = 0;
+            E_Mword = 0;
+        }
+
+       
+        cout << " Machine word: " << (double)sumt<< endl;
+        timeMachine[i-1] = (double)sumt / CLK_TCK;
+        sumt = 0;
+        countIteartion *= 10;
     }
-    
-    t = clock() - t;
-    cout << "Boolean array: " << (float)t / CLK_TCK/countIteartion << endl;
 
-    t = clock();
-    for (size_t i = 0; i < countIteartion; i++)
+    ofstream out;
+    out.open("C:\\Users\\Илья\\source\\repos\\Generarion\\Generarion\\data.txt");
+    for (size_t i = 1; i <= count; i++)
     {
-        Generator(15, A);
-        Generator(15, B);
-        Generator(15, C);
-        Generator(15, D);
-
-        A_Mword = ToMachineWord(A);
-        B_Mword = ToMachineWord(B);
-        C_Mword = ToMachineWord(C);
-        D_Mword = ToMachineWord(D);
-
-        UnionSets(A_Mword, E_Mword);
-
-        ExclusionSets(B_Mword, E_Mword);
-        ExclusionSets(C_Mword, E_Mword);
-        ExclusionSets(D_Mword, E_Mword);
-
-
-        Discharge(A);
-        Discharge(B);
-        Discharge(C);
-        Discharge(D);
-        Discharge(E);
+        out << arCount[i - 1] << ';';
     }
-    
-    t = clock() - t;
-    
-    cout << "Machine word: " << (float)t / CLK_TCK/countIteartion << endl;
+    out << endl;
 
+    for (size_t i = 1; i <= count; i++)
+    {
+        out << timeArray[i - 1] << ';';
+    }
+    out << endl;
+
+    for (size_t i = 1; i <= count; i++)
+    {
+        out << timeList[i - 1] << ';';
+    }
+    out << endl;
+
+    for (size_t i = 1; i <= count; i++)
+    {
+        out << timeBool[i - 1] << ';';
+    }
+    out << endl;
+
+    for (size_t i = 1; i <= count; i++)
+    {
+        out << timeMachine[i - 1] << ';';
+    }
+    out << endl;
 
 #pragma endregion
 
